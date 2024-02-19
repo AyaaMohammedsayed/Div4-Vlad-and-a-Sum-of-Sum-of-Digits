9aec96a759778c8ebdc23e31595864b93760caec
aya mohamed <138715179+AyaaMohammedsayed@users.noreply.github.com>
2024-02-19 20:38:50 +0200

    Update main.cpp

--- div/main.cpp
+++ div/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 
 using namespace std;
+long long  Sum(long long n)
+{
+    long long sum=0;
+    while(n>0)
+    {
+        sum+=n%10;
+        n/=10;
+    }
+    return sum;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+long long pref[200003];
+pref[0]=0;
+for(long long i=1;i<200003;i++)
+{
+    pref[i]=Sum(i);
+
+    pref[i]+=pref[i-1];
+}
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        cout<<pref[n]<<"\n";
+
+    }
+
     return 0;
 }
